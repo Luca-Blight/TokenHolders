cd64d578a86b43bffa4623ca2a1511e42f09b198
zachary_royals <royalszachary@gmail.com>
2023-04-18 22:41:06 -0400

    api developed, pipeline logic, model changes

--- app/models/TokenHolder.py
+++ app/models/TokenHolder.py
@@ -1,10 +1,7 @@
 from sqlmodel import Field, SQLModel
 from datetime import datetime
 from sqlalchemy.schema import CheckConstraint
-from sqlalchemy import Column, Sequence, Integer, BigInteger
-
-
-# what's the max integer size for the id?
+from sqlalchemy import Column, BigInteger
 
 
 class TokenHolder(SQLModel, table=True):
@@ -14,8 +11,7 @@ class TokenHolder(SQLModel, table=True):
     id: int = Field(primary_key=True)
     address: str
     balance: float = Field(index=True)
-    total_supply: int = Field(default=16969696969, sa_column=Column(BigInteger()))
-    total_supply_percentage: float = Field(default=0)
+    total_supply_percentage: float
     weekly_balance_change: float = Field(default=0)
     block_number: int
     transaction_hash: str
@@ -23,14 +19,11 @@ class TokenHolder(SQLModel, table=True):
     block_date: datetime = Field(default=datetime.utcnow)
     last_updated: datetime = Field(default=datetime.utcnow)
 
-    __table_args__ = (CheckConstraint("balance >= 0", name="balance_positive_check"),)
+    __table_args__ = (
+        CheckConstraint("balance >= 0", name="balance_positive_check"),
+        CheckConstraint(
+            "total_supply_percentage >= 0",
+            name="total_supply_percentage_positive_check",
+        ),
+    )
 
-
-# index for last_updated?
-
-
-# token holder's weekly balance change
-# % of total supply their balance represents
-
-# rollup_table for precalculations, connect by address
-# change from last record to current record
